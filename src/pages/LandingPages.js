7a65bedd46a2eb8a522965f4f6c6d9da30de2df9
khoirunalim <khoirunalim24@gmail.com>
2020-06-03 01:13:29 +0700

    first commit

---
+++ src/pages/LandingPages.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import Button from 'elements/Button';
+
+export default class LandingPages extends Component {
+  render() {
+    return <div></div>;
+  }
+}
