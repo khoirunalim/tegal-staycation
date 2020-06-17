84abc87316a8c0352397e4f377b2cf6edbe5af7a
khoirunalim <khoirunalim24@gmail.com>
2020-06-17 21:21:50 +0700

    changing hero component

--- src/pages/LandingPages.js
+++ src/pages/LandingPages.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 import Header from 'parts/Header';
+import Hero from 'parts/Hero.js';
 
+import landingPage from 'json/landingPage.json';
 export default class LandingPages extends Component {
   render() {
     return (
       <>
         <Header {...this.props}></Header>
+        <Hero data={landingPage.hero} />
       </>
     );
   }
